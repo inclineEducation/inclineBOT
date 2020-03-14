413e6b44d0ded56a21ee456e62ef1b48ba670edf
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-03-14 00:46:26 -0700

    post request handling

--- __init__.py
+++ __init__.py
@@ -1,7 +1,10 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 app = Flask(__name__)
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET', 'POST'])
 def result():
-    print(request.form['foo']) # should display 'bar'
-    return 'Received !' # response to your request.
+    if request.method == 'GET':
+        return 'get'
+    else:
+        return request.form['foo'] # should display 'bar'
+        # return data # response to your request.
 
