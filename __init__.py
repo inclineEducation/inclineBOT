6b69baf0aa215fda3d8f3d10867577000c1c8f38
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-03-18 15:24:52 -0700

    password based authentication now depracated, and fixed some exceptions

--- __init__.py
+++ __init__.py
@@ -8,16 +8,18 @@ def result():
     else:
         data = str(request.form['text'])
 	index = 0
-	while not data[index] == ' ' and  index < len(data):
+	while index < len(data) and (not data[index] == ' '):
 		index = index + 1
+	if index == len(data):
+		return 'Please enter a destination URL'
 	source = data[0:index]
 	dest = data[index + 1:len(data)]
 	if len(dest) < 8 or ((not dest[0:8] == "https://") and (not dest[0:7] == "http://")):
 		return "invalid destination string - please prefix website URLs with http:// or https:// "
-	g = Github("education.incline@gmail.com", '";3f5.Dz~MUX_8uk')
+	g = Github('b686da0712379418f91f88cf8f9e1fb2060013a0')
 	repo = g.get_repo("inclineEducation/inclineEducation.github.io")
 	redirects = repo.get_contents("_redirects")
-	repo.update_file(redirects.path, "added " + source + " -> " + dest + " to redirects",\
+	repo.update_file(redirects.path, "added http://inclineedu.org/" + source + " -> " + dest + " to redirects",\
 	str(redirects.decoded_content) + "/" + source + "    " + dest + "\n", redirects.sha)
 	return 'inclineedu.org/' + source + " now redirects to " + dest
 	return 'success'
