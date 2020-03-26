ca16cfb062880358c444019cba992a7da97dc515
Yoruio <38411921+Yoruio@users.noreply.github.com>
2020-03-26 16:56:15 -0700

    documentation and improved modularity - easy 2-step slash commands! - make your own command in less than 5 minutes!

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ from github import Github
 import os
 import requests
 import json
-from link import Link
+from link import *
 from threading import Thread
 import time
 
@@ -12,51 +12,22 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def result():
-    '''
-    post_data = {"text": "link request received"}
-    r = requests.post(
-        request.form['response_url'], data=json.dumps(post_data),
-        headers={'Content-Type': 'application/json'}
-    )
-
-    return str(r.status_code) + r.reason + " " + request.form['response_url']
-    '''
-
-    APP_ROOT = os.path.dirname(os.path.abspath(__file__))
-    tokenFile = open(os.path.join(APP_ROOT, 'token'), "r")
-    token = tokenFile.readline().rstrip('\n')
-    tokenFile.close()
     if request.method == 'GET':
         return "get method invoked - THIS URL IS USED FOR POSTING. COME BACK WITH POST. GET OUT WITH YOUR GETS."
     else:
-        print("starting new thread")
-        thread = Link(request.__copy__(), token)
-        thread.start()
-        return 'Link request received'
 
-    '''
-        data = str(request.form['text'])
-	user = None
-	try:
-		user = str(request.form['user_name'])
-	except:
-		user = 'anon'
-	index = 0
-	while index < len(data) and (not data[index] == ' '):
-		index = index + 1
-	if index == len(data):
-		return 'Please enter a destination URL'
-	source = data[0:index]
-	dest = data[index + 1:len(data)]
-	if len(dest) < 8 or ((not dest[0:8] == "https://") and (not dest[0:7] == "http://")):
-		return "invalid destination string - please prefix website URLs with http:// or https:// "
-	print('token: ' + token)
-	g = Github(str(token))
-	repo = g.get_repo("inclineEducation/inclineEducation.github.io")
-	redirects = repo.get_contents("_redirects")
-	repo.update_file(redirects.path,user + " added http://inclineedu.org/" + source + " -> " + dest + " to redirects",\
-	str(redirects.decoded_content) + "/" + source + "    " + dest + "     # added by: " + user + "\n", redirects.sha)
-	return 'inclineedu.org/' + source + " now redirects to " + dest
-	# return 'success'
-        # return data # response to your request.
-    '''
+        # dictionary mapping for slash commands:
+        # add class to link.py and add mapping switcher
+        switcher = {
+            '/link': Link
+        }
+
+        # obtain command class form switcher
+        func = switcher.get(request.form['command'], lambda: "Invalid month")
+
+        # start new command thread
+        thread = func(request.__copy__())
+        thread.start()
+
+        # confirm request received
+        return 'Link request received'
--- link.py
+++ link.py
@@ -4,16 +4,7 @@ import os
 import requests
 import json
 from threading import Thread
-
-'''
-    post_data = {"text": "link request received"}
-    r = requests.post(
-        request.form['response_url'], data=json.dumps(post_data),
-        headers={'Content-Type': 'application/json'}
-    )
-
-    return str(r.status_code) + r.reason + " " + request.form['response_url']
-'''
+import time
 
 
 def post(request, data):
@@ -24,12 +15,11 @@ def post(request, data):
 
 
 class Link(Thread):
-    def __init__(self, request, token):
+    def __init__(self, request):
         Thread.__init__(self)
         self.request = request
-        self.token = token
+
     def run(self):
-        print ("INSIDE THREAD")
         data = str(self.request.form['text'])
         user = None
         try:
@@ -37,28 +27,42 @@ class Link(Thread):
         except:
             user = 'anon'
         index = 0
+
+        # move index to end of first argument
         while index < len(data) and (not data[index] == ' '):
             index = index + 1
+
+        # if no second argument
         if index == len(data):
-            # return 'Please enter a destination URL'
             post(self.request, "Please enter a destination URL")
             return
+
+        # set destination variable
         source = data[0:index]
         dest = data[index + 1:len(data)]
-        if len(dest) < 8 or ((not dest[0:8] == "https://") and (not dest[0:7] == "http://")):
-            # return "invalid destination string - please prefix website URLs with http:// or https:// "
+
+        # check destination URL - Not valid link
+        if len(dest) < 8 or (not dest[0:8] == "https://" and not dest[0:7] == "http://"):
             post(self.request, "invalid destination string - please prefix website URLs with http:// or https:// ")
             return
-        print('token: ' + self.token)
-        g = Github(str(self.token))
+
+        # get GitHub token
+        app_root = os.path.dirname(os.path.abspath(__file__))
+        token_file = open(os.path.join(app_root, 'token'), "r")
+        token = token_file.readline().rstrip('\n')
+        token_file.close()
+
+        # open GitHub Repo
+        g = Github(str(token))
         repo = g.get_repo("inclineEducation/inclineEducation.github.io")
         redirects = repo.get_contents("_redirects")
-        repo.update_file(redirects.path, user + " added http://inclineedu.org/" + source + " -> " + dest + " to redirects", \
+
+        # update _redirects file
+        repo.update_file(redirects.path, user + " added www.inclineedu.org/" + source + " -> " + dest + " to redirects",
                          str(redirects.decoded_content) + "/" + source + "    " + dest + "     # added by: " + user + "\n",
                          redirects.sha)
+
+        # respond to /link
         post(self.request, 'inclineedu.org/' + source + " now redirects to " + dest)
         return
-        # return 'inclineedu.org/' + source + " now redirects to " + dest
-        # return 'success'
-        # return data # response to your request.
 
